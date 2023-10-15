14d34325209257354158d079669f62d212324c8d
akshaj000 <akshajsr000@gmail.com>
2023-10-15 22:13:11 +0530

    Update frontend structure

--- frontend/src/app/home/Dashboard/Login/index.tsx
+++ frontend/src/app/home/Dashboard/Login/index.tsx
@@ -8,15 +8,10 @@ const Login = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-neutral-950 bg-opacity-50 z-50 p-3 md-p-0">
       <div className="bg-neutral-900 max-w-md w-full px-4 py-6 sm:px-6 lg:px-8 rounded-md shadow-md flex flex-col justify-center items-center relative">
-        <div className="mt-12">
+        <div className="">
           <GoogleOAuthProvider clientId={clientId}>
             <GoogleOAuthButton />
           </GoogleOAuthProvider>
-          <div className="mt-4 text-center">
-            <p className="text-xs text-neutral-500">
-              This login is restricted to the internal team of AI Planet.
-            </p>
-          </div>
         </div>
       </div>
     </div>
